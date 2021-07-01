7a7238dd993519f5dc6c96bbf0ab07742b983e88
weaselflink <stefan@stefanbissell.de>
2021-07-01 09:11:42 +0200

    keep dependencies up to date

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,9 +4,10 @@ rootProject.name = "numbsi-bot"
 
 pluginManagement {
     val kotlin_version: String by settings
+    val versions_version: String by settings
     plugins {
         kotlin("jvm") version kotlin_version
-        id("com.github.ben-manes.versions") version "0.38.0"
+        id("com.github.ben-manes.versions") version versions_version
     }
 }
 
